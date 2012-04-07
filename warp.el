9547f0c84af10221349ab15f3b6eece3bdb9705e
yukihiro hara <yukihr@gmail.com>
2012-04-07 15:48:20 +0900

    comment

--- warp.el
+++ warp.el
@@ -17,9 +17,9 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-;;; Version: 0.0.1
-;;  Author: yukihiro hara <yukihr@gmail.com>
-;;  URL: http://yukihr.github.com/
+;; Author: yukihiro hara <yukihr@gmail.com>
+;; URL: http://github.com/yukihr/Warp
+;; Version: 0.0.1
 
 ;;; Contributers:
 ;;; Installation:
